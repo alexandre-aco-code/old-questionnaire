b40cb990423f205f510b3592b6ee3078dae7debd
alexandre-aco-code <77577613+alexandre-aco-code@users.noreply.github.com>
2021-01-22 15:05:47 +0100

    Add files via upload
    front amélioré, 2 modales, début du back

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
 <?php
 
-require('libraries/views/layout.html');
+require('libraries/views/layout.phtml');
 
+// chemin vers le projet 
+
+define('WWW_URL', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+
+// on prend les infos deconfiguration
+
+require_once("inc/configuration.php");
+
+
+
+try {
+
+
+    require_once("libraries/autoload.php");
+
+    \Application::process();
+
+
+
+
+
+
+} catch (Exception $e) {
+
+    \Renderer::showError([
+
+        'code' => $e->getCode(),
+        'message' => $e->getMessage()
+        
+    ]);
+
+
+
+
+}
